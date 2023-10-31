dacf373c14dc9a187ce620e2a55c951134ea4b8e
aiman <aiman.azhari@tm.com.my>
2023-10-31 09:45:07 +0800

    add column to database and add from env

--- database/migrations/2023_10_27_173213_add_google_id_user.php
+++ database/migrations/2023_10_27_173213_add_google_id_user.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('google_id')->nullable();
+            $table->string('gauth_id')->nullable();
+            $table->string('gauth_type')->nullable();
         });
     }
 
@@ -25,6 +26,9 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('gauth_id');
+            $table->dropColumn('gauth_type');
+        });
     }
 };
