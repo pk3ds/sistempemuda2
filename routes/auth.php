2aab3f1e706fbb25ddf7422214afc6257ff51328
aiman <aiman.azhari@tm.com.my>
2023-10-31 10:02:32 +0800

    add route for google

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -33,6 +34,12 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
+
+    Route::get('auth/google', [GoogleController::class, 'signInWithGoogle'])
+                ->name('signInGoogle');
+
+    Route::get('callback/google', [GoogleController::class, 'callbackToGoogle'])
+                ->name('callbackToGoogle');
 });
 
 Route::middleware('auth')->group(function () {
