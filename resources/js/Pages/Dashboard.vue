20f93f4bb0cc74134f8c4285714513c646f7849a
aiman <aiman.azhari@tm.com.my>
2024-03-20 02:14:33 +0800

    commentkan cards description

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -61,7 +61,7 @@ const formatDate = (date) => {
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> -->
                         <div class="bg-slate-600 py-3 mb-2">
                             <div class="mx-3 flex justify-between">
                                 <h3 class="text-white">RM 200</h3>
